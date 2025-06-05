e96219e985e366f86c9036470bb3186facf8458c
Rudolfs Pukitis <pukitis.rudolfs@gmail.com>
2025-06-05 13:42:05 +0300

    adds complete test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -83,4 +83,41 @@ describe('App Navigation', () => {
       ),
     ).toBeInTheDocument()
   })
+
+  it('should complete the test', async () => {
+    const user = userEvent.setup()
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    )
+    const nameInput = screen.getByPlaceholderText('Enter your name');
+    fireEvent.change(nameInput, { target: { value: 'John Doe' } })
+    const select = screen.getByTestId('select-trigger')
+    await user.click(select)
+    const option = screen.getAllByText('React Fundamentals')
+    await user.click(option[1])
+    const submitButton = screen.getByText('Start Test')
+    fireEvent.click(submitButton)
+
+    let answers = screen.getAllByRole('radio')
+    await user.click(answers[2])
+    let nextButton = screen.getByText('Next')
+    fireEvent.click(nextButton)
+    screen.debug()
+
+    answers = screen.getAllByRole('radio')
+    await user.click(answers[1])
+    nextButton = screen.getByText('Next')
+    fireEvent.click(nextButton)
+
+    answers = screen.getAllByRole('radio')
+    await user.click(answers[0])
+    nextButton = screen.getByText('Finish')
+    fireEvent.click(nextButton)
+
+    expect(screen.getByText('Congratulations, John Doe!')).toBeInTheDocument()
+    expect(screen.getByText('100%')).toBeInTheDocument()
+    expect(screen.getByText('You have answered 3 out of 3 questions correctly.')).toBeInTheDocument()
+  })
 })
